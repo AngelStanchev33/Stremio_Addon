bce8f613b0e4017cf07f89998f5f404f2818410c
AngelStanchev <angelstanchev33@gmail.com>
2026-01-20 14:44:01 -0500

    Stremio Addon test

---
+++ src/main/java/org/example/stremioaddon/Stream.java
@@ -0,0 +1,6 @@
+package org.example.stremioaddon;
+
+public class Stream {
+
+
+}
---
+++ src/test/java/org/example/stremioaddon/StremioAddonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stremioaddon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StremioAddonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
